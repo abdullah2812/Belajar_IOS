dd0433a4abb1d5e14702b1589946af4d6812a673
Abdullah <iriandaabdullah546@gmail.com>
2022-08-11 20:39:31 +0700

    pertama

---
+++ Belajar_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Belajar_IOS
+//
+//  Created by Irianda on 11/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
